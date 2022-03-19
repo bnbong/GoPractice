52e66f2d5ceecf91243bc6df82d95eb56e717305
bnbong <bbbong9@gmail.com>
2022-03-19 08:59:05 +0000

    project banking: finish up

--- main.go
+++ main.go
@@ -9,11 +9,5 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		// log.Fatalln(err) // kills program
-		fmt.Print(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
