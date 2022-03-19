6f697d3609d09630b1cfb187511fda7f87263056
bnbong <bbbong9@gmail.com>
2022-03-19 07:43:55 +0000

    learned struct

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age int
+	favFood []string
+}
+
+// not exist at Go:
+// constructor()
+// __init__
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"} //map[key]value
-	for key, _ := range nico {
-		fmt.Println(key)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name:"nico", age: 18, favFood: favFood}
+	fmt.Println(nico.name)
 }
