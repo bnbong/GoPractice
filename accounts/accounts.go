9ad449e27fdad05290994f02427c384ae0662771
bnbong <bbbong9@gmail.com>
2022-03-19 08:52:48 +0000

    project banking: methods part two

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,12 +1,17 @@
 package accounts
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Account struct
 type Account struct {
 	owner string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -15,12 +20,21 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) { // receiver function(method)
+func (a *Account) Deposit(amount int) { // receiver function(method)
 	fmt.Println("Gonna deposit", amount)
-	a.balance += amount // will solve next time
+	a.balance += amount // solved with adding * at receiver
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil // == none
 }
--- main.go
+++ main.go
@@ -10,4 +10,10 @@ func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		// log.Fatalln(err) // kills program
+		fmt.Print(err)
+	}
+	fmt.Println(account.Balance())
 }
